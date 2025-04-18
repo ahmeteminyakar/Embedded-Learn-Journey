bd665ad65dced8881027353fc756fb677295439f
ahmeteminyakar <ahmeteminyakar@gmail.com>
2025-04-18 22:35:22 +0300

    📁 Organized Python projects into /python_projects folder

---
+++ python_projects/Day1_PythonBasics.py
@@ -0,0 +1,12 @@
+print("Welcome")
+
+age=int(input("Please enter your age : "))
+
+if age>=18:
+
+    print("You can get a driver licence!")
+
+
+else:
+
+    print("You can't get a driver licence!")    
