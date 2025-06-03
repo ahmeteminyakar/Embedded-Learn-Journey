50a68dd3e6bf089234942939a32bd9a1d2e23f33
ahmeteminyakar <ahmeteminyakar@gmail.com>
2025-06-03 17:37:08 +0300

    Updated Folder Structure

--- python_projects/uart_logging/uart_logging.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-co2 = 805.5
-timestamp = datetime.now().strftime("%H:%M:%S")
-
-with open("co2_log.csv", "a") as f:
-    f.write(f"{timestamp},{co2}\n")
-
-print("✅ CO2 reading logged.")
